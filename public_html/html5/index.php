d8953f4fdbf62ac5a31bc2953eb6de4d492330cd
Gerson Felipe Schwinn <gerson@onehost.com.br>
2018-04-21 09:50:47 -0300

    Atualizando

---
+++ public_html/html5/index.php
@@ -0,0 +1,18 @@
+<?php
+$this->response->header('Link: <jquery.validate.min.js>; rel=preload; as=script');
+$this->response->header('Link: <jquery.min.js>; rel=preload; as=script');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="jquery.min.js"></script>
+    <script src="jquery.validate.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
